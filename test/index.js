d13502710ec05288e75ab2e25043762df4ec0ed9
Martin Heidegger <martin.heidegger@gmail.com>
2016-09-26 18:41:56 +0900

    Unsubscribing should work in the given order

--- test/index.js
+++ test/index.js
@@ -26,3 +26,34 @@ test('should pass new value to subscribers', function (t) {
   })
   o(1)
 })
+
+test('should allow to unsubscribe while listening', function (t) {
+  t.plan(1)
+  var o = Observable(0)
+  var order = []
+  var record = function (name) {
+    return function (val) {
+      order.push({
+        name: name,
+        val: val
+      })
+    }
+  }
+  o.subscribe(record('A'))
+  var unsubscribe = o.subscribe(function (val) {
+    record('B')(val)
+    unsubscribe()
+    setImmediate(function () {
+      o(2)
+      t.deepEqual(order, [
+        {name: 'A', val: 1},
+        {name: 'B', val: 1},
+        {name: 'C', val: 1},
+        {name: 'A', val: 2},
+        {name: 'C', val: 2}
+      ])
+    })
+  })
+  o.subscribe(record('C'))
+  o(1)
+})
